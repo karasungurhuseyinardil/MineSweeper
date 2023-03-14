a8c2b78095a0ec84c084bfcb7d0a33e754dc28ae
Hüseyin Ardıl Karasungur <72706704+karasungurhuseyinardil@users.noreply.github.com>
2023-03-14 09:05:52 +0300

    Update Main.java

--- minesweeper/Main.java
+++ minesweeper/Main.java
@@ -1,36 +1,52 @@
-package  minesweeper;
+package minesweeper;
 import java.util.InputMismatchException;
 import java.util.Scanner;
 
-
 public class Main {
-        public static void main(String[] args){
-        
-            Scanner input=new Scanner(System.in);
-            try{
-            
-        
+  public static void main(String[] args) {
+    boolean isTrue = true;
+    Scanner input = new Scanner(System.in);
+    
+    do {
+      try {
+
         System.out.println("Welcome!!!");
-            
+
         System.out.println("Please enter the dimensions!!!");
         System.out.println("Number of Rows::");
-        int row=input.nextInt();
+        int row = input.nextInt();
+        
         System.out.println("Number of Columns::");
-         int column=input.nextInt();
-    
-    MineSweeper mine=new MineSweeper (row,column);
-    mine.run();}catch(NegativeArraySizeException e){
-            System.out.println("Negative Value");
-    input.nextInt();}
-        catch(InputMismatchException e)
-    {System.out.println("Wrong index type");
-    input.nextInt();}finally {System.out.println("Finished");
-        }
-  
-Question myquestion=new Question();
-myquestion.setQuestion("DO YOU LIKE MY GAME?");
-            System.out.println(myquestion.getQuestion());
-           
-    }
-        }
- 
+        int column = input.nextInt();
+ if(row==0&&column==0){
+                System.out.println("Use bigger row and column values !!!");
+            continue;}
+            if(row==0){
+            System.out.println("Use bigger row value!!");
+            continue;}
+            if(column==0){
+            System.out.println("Use bigger column value!!");
+            continue;}
+        
+        MineSweeper mine = new MineSweeper(row, column);
+        mine.run();
+        isTrue = true;
+      } catch (NegativeArraySizeException e) {
+        System.out.println("Negative Value");
+               
+        input.next();
+      } catch (InputMismatchException e) {
+        System.out.println("Wrong index type");
+        input.next();
+      } catch (ArrayIndexOutOfBoundsException e) {
+        System.out.println("Array out of bound");
+        input.next();
+       
+      }
+      
+    } while (isTrue);
+   Question myquestion = new Question();
+    myquestion.setQuestion("DO YOU LIKE MY GAME?");
+    System.out.println(myquestion.getQuestion());
+  }
+}
